5d91bd6df2d30fed4b5e38b5a6065a21019ade06
Joshua Garren <garrenjl@gmail.com>
2023-02-18 11:25:15 -0600

    Tested to make sure still working and updated README

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,8 @@
 
 export const environment = {
   production: false,
-  API: "http://192.168.1.168:8080/api/v1/",
-  ImgFilePath: "http://192.168.1.215/web_images/"
-  //API: "http://192.168.1.69:8080/api/v1/"
+  API: "http://127.0.0.1:8080/api/v1/",
+  ImgFilePath: "http://192.168.1.216/web_images/"
 };
 
 /*
